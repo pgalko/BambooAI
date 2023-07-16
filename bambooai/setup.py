646cd896ab90bf43ae2914abe6deb1be642ccb5c
pgalko <39939157+pgalko@users.noreply.github.com>
2023-07-16 18:34:30 +1000

    Create setup.py

---
+++ bambooai/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='bambooai',
+    version='0.3.11',
+    description='A lightweight library for working with pandas dataframes using natural language queries',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Palo Galko',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'tiktoken',
+        'pandas',
+        'termcolor',
+        'newspaper3k',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
